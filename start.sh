ff8e88badabac2b9ca12a051cc3fd1fa129a27b5
davidbuenov <buenovallejo@gmail.com>
2026-06-13 18:37:05 +0200

    docs: configurar arranque inteligente para evitar reinstalar dependencias cada vez

--- start.sh
+++ start.sh
@@ -9,16 +9,20 @@
 echo "Iniciando aplicación dbv-md2word..."
 
 if [ ! -d "venv" ]; then
+    echo "[CONFIGURACIÓN] Es la primera vez que ejecutas la aplicación."
     echo "Creando entorno virtual..."
     python3 -m venv venv
+    
+    echo "Activando entorno virtual..."
+    source venv/bin/activate
+    
+    echo "Instalando/Actualizando dependencias (este proceso solo ocurre la primera vez)..."
+    pip install --upgrade pip
+    pip install -r requirements.txt
+else
+    echo "Activando entorno virtual..."
+    source venv/bin/activate
 fi
-
-echo "Activando entorno virtual..."
-source venv/bin/activate
-
-echo "Verificando dependencias..."
-pip install --upgrade pip
-pip install -r requirements.txt
 
 echo "Iniciando servidor local..."
 python server.py
